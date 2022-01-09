13754d9e00106359656ae32e361c03ad5aec9663
Alvaro Ramirez Lopez(Alvaro-Desktop-Linux) <alvaro_rl@ciencias.unam.mx>
2022-01-09 00:06:17 -0600

    se agrego la ruta principal

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ const app = express();
 // configuraciones
 app.set('port', 4000)
 
+// rutas
+app.get('/',(req,res) => {
+    res.send('Bienvenido');
+})
+
 // servidor escuchando
 app.listen(app.get('port'), () => {
     console.log('Servidor escuchando en el puerto',app.get('port'));
