0c0db896b2118ec4d333444c128edfe178b3bd55
Alvaro Ramirez Lopez(Alvaro-Desktop-Linux) <alvaro_rl@ciencias.unam.mx>
2022-01-08 22:58:14 -0600

    configuraciones y listening

--- src/index.js
+++ src/index.js
@@ -2,3 +2,11 @@
 const express = require('express');
 // constante principal del programa
 const app = express();
+
+// configuraciones
+app.set('port', 4000)
+
+// servidor escuchando
+app.listen(app.get('port'), () => {
+    console.log('Servidor escuchando en el puerto',app.get('port'));
+})
